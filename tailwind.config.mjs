1b6e542df2ab72225dca5d2c6de93c93ccfc3b71
Pablo-ArgF <pablo.af.important@gmail.com>
2025-10-31 19:44:15 +0100

    Sección principal funcionando, empiezo con contenido

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7e22ce',
+        secondary: '#080808',
+        outlineColor: '#1F2123',
+      },
+      fontFamily: {
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+        'dancing': ['Dancing Script', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+
